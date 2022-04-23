e096ee6d26dfb567e722735ca74124d0d6a8404d
rodrigoaqueiroz <rodrigoandradequeiroz@gmail.com>
2022-04-23 12:34:57 -0300

    feat: req4.4

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,19 +1,36 @@
 def is_anagram(first_string, second_string):
-    return bubble_sort(
-        list(first_string.lower())) == bubble_sort(list(second_string.lower()))
+    return merge_sort(
+        list(first_string.lower())) == merge_sort(list(second_string.lower()))
 
 
-def bubble_sort(word):
-    has_swapped = True
-    count = 0
-    while has_swapped:
-        has_swapped = False
-        for index in range(len(word) - count - 1):
-            if word[index] > word[index + 1]:
-                word[index], word[index + 1] = word[index + 1], word[index]
-                has_swapped = True
-        count += 1
+def merge_sort(array):
+    if len(array) <= 1:
+        return array
+    mid = len(array) // 2
+    left, right = merge_sort(array[:mid]), merge_sort(array[mid:])
+    return merge(left, right, array.copy())
 
-    return word
 
-# Referêcia bubble_sort: Conteúdo da Trybe
+def merge(left, right, merged):
+    left_cursor, right_cursor = 0, 0
+    while left_cursor < len(left) and right_cursor < len(right):
+        if left[left_cursor] <= right[right_cursor]:
+            merged[left_cursor + right_cursor] = left[left_cursor]
+            left_cursor += 1
+        else:
+            merged[left_cursor + right_cursor] = right[right_cursor]
+            right_cursor += 1
+    for left_cursor in range(left_cursor, len(left)):
+        merged[left_cursor + right_cursor] = left[left_cursor]
+    for right_cursor in range(right_cursor, len(right)):
+        merged[left_cursor + right_cursor] = right[right_cursor]
+    return merged
+
+
+'''
+merge_sort:
+(é um algoritmo de ordenação que usa dividir e conquistar,
+tem mais eficiência do que o bubble sort neste caso.)
+Referência - Conteúdo do bloco:
+https://app.betrybe.com/course/computer-science/algoritmos/algoritmos-de-ordenacao-e-busca/29521083-44ea-488d-a74d-216b1ac79b04/conteudos/766d59bd-a04f-41a9-b41b-5fb37a2f2a3a/algoritmos-de-ordenacao/1adc1a6e-51c7-4065-ae2c-e5f8194e7578?use_case=side_bar
+'''

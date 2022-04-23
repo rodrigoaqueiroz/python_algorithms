48e18e64188129f5eb410b904a8218dea260c8ec
rodrigoaqueiroz <rodrigoandradequeiroz@gmail.com>
2022-04-23 11:45:04 -0300

    feat: req04

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,11 @@
 def find_duplicate(nums):
-    """ Faça o código aqui. """
+    nums.sort()
+    try:
+        if len(nums) < 2 or nums[0] < 1:
+            return False
+        for index in range(len(nums)-1):
+            if nums[index] == nums[index+1]:
+                return nums[index]
+        return False
+    except (TypeError, ValueError):
+        return False

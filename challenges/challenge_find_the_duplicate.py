e64ce7b31f04c5b8b57547ce202af38b985f6b4f
rodrigoaqueiroz <rodrigoandradequeiroz@gmail.com>
2022-04-23 11:50:35 -0300

    fix: decrease challenge_find_the_duplicates complexity - 6 to 5

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,11 +1,10 @@
 def find_duplicate(nums):
     nums.sort()
-    try:
-        if len(nums) < 2 or nums[0] < 1:
+    if len(nums) < 2:
+        return False
+    for index in range(len(nums)-1):
+        if type(nums[index]) is str or nums[0] < 1:
             return False
-        for index in range(len(nums)-1):
-            if nums[index] == nums[index+1]:
-                return nums[index]
-        return False
-    except (TypeError, ValueError):
-        return False
+        if nums[index] == nums[index+1]:
+            return nums[index]
+    return False

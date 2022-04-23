e033f3b79acf339772344886d185c77d27e05986
rodrigoaqueiroz <rodrigoandradequeiroz@gmail.com>
2022-04-23 11:14:13 -0300

    feat: req03 (subreqs 1,2,3,5)

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,3 +1,19 @@
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
-    """first commit"""
+    return bubble_sort(
+        list(first_string.lower())) == bubble_sort(list(second_string.lower()))
+
+
+def bubble_sort(word):
+    has_swapped = True
+    count = 0
+    while has_swapped:
+        has_swapped = False
+        for index in range(len(word) - count - 1):
+            if word[index] > word[index + 1]:
+                word[index], word[index + 1] = word[index + 1], word[index]
+                has_swapped = True
+        count += 1
+
+    return word
+
+# Referêcia bubble_sort: Conteúdo da Trybe
--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,9 +1,9 @@
 def study_schedule(permanence_period, target_time):
     try:
-        cont = 0
+        count = 0
         for period in permanence_period:
             if period[0] <= target_time <= period[1]:
-                cont += 1
+                count += 1
     except TypeError:
         return None
-    return cont
+    return count

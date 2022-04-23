1546e461b40eecb773bb7c7d3f50f404dad67cdb
rodrigoaqueiroz <rodrigoandradequeiroz@gmail.com>
2022-04-23 08:34:06 -0300

    feat: req01

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,9 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    try:
+        cont = 0
+        for period in permanence_period:
+            if period[0] <= target_time <= period[1]:
+                cont += 1
+    except TypeError:
+        return None
+    return cont

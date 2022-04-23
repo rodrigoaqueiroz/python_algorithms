073d8f3212f259163547e42454454f6e0fa3dfd2
rodrigoaqueiroz <rodrigoandradequeiroz@gmail.com>
2022-04-23 10:15:40 -0300

    feat: req02

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,9 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+    try:
+        if word[low_index] != word[high_index]:
+            return False
+        if low_index == high_index or low_index == high_index - 1:
+            return True
+    except IndexError:
+        return False
+    return is_palindrome_recursive(word, low_index + 1, high_index - 1)
